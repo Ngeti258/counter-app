48924ae3e2bd93cb3658a48d377742e43531aaeb
Ngeti <70070716+Ngeti258@users.noreply.github.com>
2022-07-19 22:53:11 +0300

    Create loginform.jsx

---
+++ src/loginform.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+
+class LoginForm extends Component {
+    state = { 
+        data:{username:'',password:''},
+        error:{}
+     } 
+    render() { 
+        return (
+
+        );
+    }
+}
+ 
+export default LoginForm;
